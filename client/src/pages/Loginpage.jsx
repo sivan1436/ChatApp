930d53dec6005f9cd1a45b1fc5ba575942af3f57
siva <23ht1a4374@gmail.com>
2026-09-08 02:34:41 +0530

    update:login page feature update

--- client/src/pages/Loginpage.jsx
+++ client/src/pages/Loginpage.jsx
@@ -8,13 +8,21 @@ const Lofinpage = () => {
   const [password,setPassword] = useState("")
   const [Bio,setBio] = useState("")
   const [isDataSubmited,setIsDataSubmited] = useState(false)
+  async function onSubmitHandler(e) {
+    e.preventDefault();
+    if(currentstate ==="Sign up" && !isDataSubmited){
+      setIsDataSubmited(true)
+    }
+    
+  }
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center
     justify-content gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
      {/* left */}
      <img src={assets.logo_big} alt='' className='w-[min(30vw,250px)]' />
         {/* Right side */}
-       <form className='border-2 bg-white/8 text-white border-gray-500 p-6 
+       <form onSubmit={onSubmitHandler}
+       className='border-2 bg-white/8 text-white border-gray-500 p-6 
        flex flex-col gap-6 rounded-lg shadow-lg'>
         <h2 className='font-medium text-2xl flex justify-between items-center'>
         {currentstate}
