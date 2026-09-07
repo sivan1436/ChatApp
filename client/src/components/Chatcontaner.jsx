f5d52b618c45ef6bfd827a234c2f2d45979527de
siva <23ht1a4374@gmail.com>
2026-09-07 21:38:44 +0530

    feat : chat area page build

--- client/src/components/Chatcontaner.jsx
+++ client/src/components/Chatcontaner.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import assets from '../assets/assets'
+import React, { useEffect, useRef } from 'react'
+import moment from "moment"
+import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/utils'
 
 const Chatcontaner = ({selecteduser,setSelecteduser}) => {
+    const scrollEnd = useRef()
+    useEffect(()=>{
+      if (scrollEnd){
+        scrollEnd.current?.scrollIntoView({behavior : "smooth"})
+      }
+    },[])
+     
   return selecteduser ? (
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+      {/* heasder */}
       <div className='flex items-center gap-3 py-3 mx-4
        border-b border-stone-500'>
         <img src={assets.profile_martin}
@@ -16,6 +26,51 @@ const Chatcontaner = ({selecteduser,setSelecteduser}) => {
         <img src={assets.help_icon} className='max-md:hidden max-w-5' />
 
       </div>
+      {/* chat area */}
+      <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3
+      pb-6'>
+        {messagesDummyData.map((msg,index)=>(
+
+          <div key={index}
+          className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
+            {msg.image ? (<img src={msg.image}
+            className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8'/>)
+            : (
+              <p className={`p-2 max-w-[200px] md:text-sm font-light
+              rounded-lg mb-8 break-all bg-violet-500/30 text-white${msg.senderId=== '680f50e4f1cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>
+                {msg.text}
+              </p>
+            )}
+            <div className='text-center text-xs'>
+              <img src={ msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt=''
+              className='w-7 rounded-full'/>
+              <p className='text-gray-500'>{formatMessageTime(msg.createdAt)}</p>
+
+            </div>
+          </div>
+        ))}
+        <div ref={scrollEnd}>
+
+        </div>
+      </div>
+     {/* bottom area */}
+     <div className='absolute bottom-0 left-0 right-0 
+     flex items-center gap-3 p-3'>
+      <div className='flex-1 flex items-center bg-gray-100/12 px-3
+      rounded-full'>
+        <input type='text'
+        className='flex-1 text-sm p-3 border-none rounded-lg outline-none 
+        text-white placeholder-gray-400'
+        placeholder='send a message' />
+        <input type='file' id='media' accept='image/*,video/*' hidden />
+        <label htmlFor='media'>
+          <img src ={assets.gallery_icon} alt ="" 
+          className="w-5 mr-2 cursor-pointer"/>
+        </label>
+      </div>
+      <img src={assets.send_button} alt='' className='w-7 cursor-pointer' />
+     </div>
+
     </div>
   ) : (
     <div className='flex flex-col items-center justify-center gap-2 text-gray-500
